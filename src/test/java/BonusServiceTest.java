3f1a9f596d7360111a16511f92dc9d87172e0161
Olga Brazhkina <emelyanova84@yandex.ru>
2022-12-06 01:16:02 +0500

    Initial commit. Task done

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,56 +1,14 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class BonusServiceTest {
 
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    public void testRegisteredLimit(long amount, boolean registered, int expected) {
         BonusService service = new BonusService();
-
-        long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
-
         long actual = service.calculate(amount, registered);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1_000_000;
-        boolean registered = true;
-        long expected = 500;
-
-        long actual = service.calculate(amount, registered);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForUnregisteredAndUnderLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1000;
-        boolean registered = false;
-        long expected = 10;
-
-        long actual = service.calculate(amount, registered);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForUnregisteredAndOverLimit() {
-        BonusService service = new BonusService();
-        long amount = 1_000_000;
-        boolean registered = true;
-        long expected = 500;
-
-        long actual = service.calculate(amount, registered);
-
         Assertions.assertEquals(expected, actual);
     }
 }
